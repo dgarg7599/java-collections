95ae6b524bbdeb993ecb3f9238ae28c61a0e5e01
dgarg7599 <gargdivyansh7599@gmail.com>
2025-03-29 00:39:09 +0530

    [Divyansh Garg] ADD: Implemented a circular buffer (fixed-size queue) using an array-based queue.

---
+++ src/main/java/com/bridgelabz/queueinterface/CircularBuffer.java
@@ -0,0 +1,43 @@
+package com.bridgelabz.queueinterface;
+
+class CircularBuffer {
+    private int[] buffer;
+    private int head, tail, size, capacity;
+
+    public CircularBuffer(int capacity) {
+        this.capacity = capacity;
+        this.buffer = new int[capacity];
+        this.head = 0;
+        this.tail = 0;
+        this.size = 0;
+    }
+
+    public void insert(int value) {
+        buffer[tail] = value;
+        tail = (tail + 1) % capacity;
+        if (size < capacity) {
+            size++;
+        } else {
+            head = (head + 1) % capacity;
+        }
+    }
+
+    public void printBuffer() {
+        System.out.print("Buffer: [");
+        for (int i = 0; i < size; i++) {
+            System.out.print(buffer[(head + i) % capacity]);
+            if (i < size - 1) System.out.print(", ");
+        }
+        System.out.println("]");
+    }
+
+    public static void main(String[] args) {
+        CircularBuffer cb = new CircularBuffer(3);
+        cb.insert(1);
+        cb.insert(2);
+        cb.insert(3);
+        cb.printBuffer();
+        cb.insert(4);
+        cb.printBuffer();
+    }
+}
